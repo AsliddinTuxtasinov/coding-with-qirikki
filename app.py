edaa81bacaf0acae8c620a0e00a992f399b51586
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2023-12-22 16:02:07 +0500

    2-create blog web app

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
+import os
+from slugify import slugify
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
+def get_articles():
+    articles = os.listdir("articles")
+    slug_article = {}
+
+    for article in articles:
+        slug = slugify(article)
+        slug_article[slug] = article
+
+    return slug_article
+
+
 @app.route('/')
 def home():
-    return f"Welcome to Home page"
+    articles = get_articles()
+    return render_template("index.html", articles=articles)
+
+
+@app.route('/blog/<slug>')
+def blog_detail(slug: str):
+    articles = get_articles()
+
+    try:
+        title = articles[slug]
+    except Exception as e:
+        print(f"Error: {e}")
+        return f"Object not found"
+
+    with open(f"articles/{title}") as f:
+        content = f.read()
+
+    return render_template("article-detail.html", title=title, content=content)
 
 
 @app.route('/add', methods=['POST', 'GET'])
